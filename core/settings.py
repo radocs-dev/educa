8cd3728ff3b71fbd5d78850007dc16b1382bd76f
Rafael Silva <radocs.dev@gmail.com>
2025-09-29 22:29:52 -0300

    Courses app models created

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'courses',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Custom settings
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
